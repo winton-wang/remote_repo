7ec7b48720a8a61d59f9737634c65768347ec43e
Winton Wang <winton168@hotmail.com>
2020-06-21 17:22:15 -0400

    divide

--- calc.py
+++ calc.py
@@ -1,12 +1,12 @@
 def add(x, y):
-    pass
+    return x + y
 
 def sub(x, y):
     pass
 
 def multiple(x, y):
-    return x * y 
+    return x*y 
 
 def divide(x, y):
-     return x / y
-     
+     return x/y
+
